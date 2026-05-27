0b231ca80d6b640499b7b3dd15e42f72358b6d85
Sofia Bacher <bacher.sofiaet36@gmail.com>
2026-05-26 23:03:38 -0300

    Update Auth.jsx

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -47,7 +47,7 @@ const LoginSignUp = () => {
         }
 
         try {
-            const response = await axios.post('http://localhost:3000/auth/recover-password', { email })
+            const response = await axios.post('/auth/recover-password', { email })
             showSnackbar(response.data.message, "success")
             handleChangeAction("Iniciar sesion")
 
