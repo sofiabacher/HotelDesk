7d34635139658c6108d97c7b61fb6fa14d17c285
Sofia Bacher <bacher.sofiaet36@gmail.com>
2026-05-26 21:26:37 -0300

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     logging: false,
+
     dialectOptions: {
         ssl: {
             require: true,
