7962bccad2f2c146ae911562710317830ec9df27
Sofia Bacher <bacher.sofiaet36@gmail.com>
2026-05-27 01:33:14 -0300

    Update cors.js

--- api/middlewares/cors.js
+++ api/middlewares/cors.js
@@ -3,19 +3,18 @@ const { cors } = require('../config')
 
 module.exports = (req, res, next) => {
     const origin = req.headers.origin;
+
     const allowedOrigins = [
-        cors.origin,
+        'https://hotel-desk-ten.vercel.app',
         'http://localhost:5173'
     ];
 
-    const cleanOrigin = origin?.replace(/\/$/, '');
-    const isAllowed = allowedOrigins.some(o => o?.replace(/\/$/, '') === cleanOrigin);
+    const isAllowed = allowedOrigins.includes(origin);
 
     if (isAllowed) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
 
-    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS') //Métodos HTTP permitidos
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization') //Headers permitiidos desde frontend (JSON en body y token de autenticación)
     res.setHeader('Access-Control-Allow-Credentials', 'true') //Permite enviar cookies o tokens en peticiones cross-origin
