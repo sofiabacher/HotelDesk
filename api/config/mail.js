4007c59378172ec438b975dfa295dab3f731e062
Sofia Bacher <bacher.sofiaet36@gmail.com>
2026-05-27 01:43:33 -0300

    Update mail.js

--- api/config/mail.js
+++ api/config/mail.js
@@ -2,23 +2,10 @@ const nodeMalier = require('nodemailer')
 const { email } = require('../config')
 
 const transporter = nodeMalier.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // 🔥 clave (SSL directo)
+    service: "gmail",
     auth: {
         user: email.user,
         pass: email.password
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-})
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.log("SMTP ERROR:", error)
-    } else {
-        console.log("SMTP READY")
     }
 })
 
