80edda7322e19fc276c8155599cfea6c0258f068
Sofia Bacher <bacher.sofiaet36@gmail.com>
2026-05-27 00:08:18 -0300

    Update cors.js

--- api/middlewares/cors.js
+++ api/middlewares/cors.js
@@ -2,7 +2,20 @@
 const { cors } = require('../config')
 
 module.exports = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', cors.origin) //Permite que solo este frontend pueda hacer peticiones 
+    const origin = req.headers.origin;
+    const allowedOrigins = [
+        cors.origin,
+        'http://localhost:5173'
+    ];
+
+    const cleanOrigin = origin?.replace(/\/$/, '');
+    const isAllowed = allowedOrigins.some(o => o?.replace(/\/$/, '') === cleanOrigin);
+
+    if (isAllowed) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS') //Métodos HTTP permitidos
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization') //Headers permitiidos desde frontend (JSON en body y token de autenticación)
     res.setHeader('Access-Control-Allow-Credentials', 'true') //Permite enviar cookies o tokens en peticiones cross-origin
